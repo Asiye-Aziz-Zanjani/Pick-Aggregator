ae302a53a4105afa7bda568d12fa1cfaa38d336f
Asiye-Aziz-Zanjani <156545917+Asiye-Aziz-Zanjani@users.noreply.github.com>
2026-05-28 11:29:28 -0600

    Update pick_aggregator.py

--- picker_aggregator.py
+++ picker_aggregator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Picker Aggregator: Multi-model Machine Learning Picker to Develop Ensemble Earthquake Catalog
+Pick Aggregator: Multi-model Machine Learning Picker to Develop Ensemble Earthquake Catalog
 
 Developed by: Asiye Aziz Zanjani, 2025
 
